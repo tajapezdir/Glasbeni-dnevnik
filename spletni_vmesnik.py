69a9825e5efecda795cfcab6ebb5c92efa9d49aa
Taja Pezdir <tajaa.pezdir@gmail.com>
2020-08-30 18:26:45 +0200

    dodala funkcijo spletnemu vmesniku

--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -1,26 +1,35 @@
 import bottle
-from model import Dnevnik, seznam_zanrov
+from model import Dnevnik, seznam_zanrov, seznam_ocen
 import json
 
-from datetime import time
+from datetime import date
 
 JSONOVA_DATOTEKA = 'dnevnik.json'
 
 prvi_dnevnik = Dnevnik.nalozi_stanje(JSONOVA_DATOTEKA)
 
+
+# GET DEKORATORJI
+
 @bottle.get('/')
 def zacetna_stran():
-    return bottle.template('zacetna_stran.html', dnevnik=prvi_dnevnik)
+    return bottle.template('zacetna_stran.html', dnevnik=prvi_dnevnik, zvrsti=seznam_zanrov, ocene=seznam_ocen)
 
-# @bottle.post('/dodaj-album/')
-# def dodaj_preliv():
-#     znesek = int(bottle.request.forms['znesek'])
-#     datum = date.today().strftime('%Y-%m-%d')
-#     racun = poisci_racun('racun')
-#     kuverta = poisci_kuverto('kuverta')
-#     opis = bottle.request.forms.getunicode('opis')
-#     proracun.nov_preliv(znesek, datum, opis, racun, kuverta)
-#     shrani_trenutnega_uporabnika()
-#     bottle.redirect('/')
+# POST DEKORATORJI
+
+@bottle.post('/dodaj-album/')
+def nov_album():
+    naslov = bottle.request.forms.getunicode('naslov')
+    izvajalec = bottle.request.forms.getunicode('izvajalec')
+    datum = date.today() 
+    leto_izdaje = int(bottle.request.forms.getunicode('leto izdaje'))
+    zvrst = bottle.request.forms.getunicode('zvrst')
+    ocena = int(bottle.request.forms.getunicode('ocena'))
+    opis = bottle.request.forms.getunicode('opis')
+    prvi_dnevnik.nov_album(naslov, izvajalec, datum, leto_izdaje, zvrst, ocena, opis)
+    bottle.redirect('/')
+
+
+
 
 bottle.run(debug=True, reloader=True)
